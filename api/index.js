9418deb439e9a59f13251e46fb0c661232dd97f1
BhushanC23 <bhushan.chaturbhuj_25pca@sanjivani.edu.in>
2025-12-01 16:26:45 +0530

    Serve frontend static files from backend

--- api/index.js
+++ api/index.js
@@ -5,8 +5,13 @@ import dotenv from 'dotenv';
 import connectDB from './config/db.js';
 import shareRoutes from './routes/shareRoutes.js';
 import errorHandler from './middleware/errorHandler.js';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 dotenv.config();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
@@ -15,13 +20,23 @@ connectDB();
 
 // Middleware
 app.use(cors());
-app.use(helmet());
+app.use(helmet({
+  contentSecurityPolicy: false, // Disable CSP for simple static serving
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// Serve Static Files (Frontend)
+app.use(express.static(path.join(__dirname, '../ziply')));
 
 // Routes
 app.get('/api', (req, res) => res.send('Ziply API Running 🚀'));
 app.use('/api/share', shareRoutes);
+
+// Serve index.html for root
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../ziply/index.html'));
+});
 
 // Error Handler
 app.use(errorHandler);
